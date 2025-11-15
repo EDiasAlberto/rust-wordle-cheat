51d8d4d3eca67cceec2dd0018563b1e9a9a64911
Ethan Dias Alberto <proxamon1@gmail.com>
2025-11-15 22:28:08 +0000

    feat: implement rough overall pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use serde::Deserialize;
-use std::error::Error;
+
+const WORDLE_URL: &str = "https://www.nytimes.com/svc/wordle/v2/2025-11-15.json";
+
+const CONNECTIONS_URL: &str = "https://www.nytimes.com/svc/connections/v2/2025-11-15.json";
 
 #[derive(Deserialize, Debug)]
 struct WordleResp {
@@ -10,11 +13,51 @@ struct WordleResp {
     editor: String
 }
 
+#[derive(Deserialize, Debug)]
+struct WordChoices {
+    content: String,
+    position: u8,
+}
+
+#[derive(Deserialize, Debug)]
+struct ConnectionsCategories {
+    title: String,
+    cards: Vec<WordChoices>,
+}
+
+#[derive(Deserialize, Debug)]
+struct ConnectionsResp {
+    id: u32,
+    print_date: String,
+    editor: String,
+    categories: Vec<ConnectionsCategories>,
+}
+
+#[derive(Deserialize, Debug)]
+struct WebResponse {
+    connections: ConnectionsResp,
+    wordle: WordleResp,
+}
+
+//fn get_solution(game: char) -> Result<WebResponse, Box<dyn std::error::Error>> {
+fn get_solution(game: char) -> Result<(), Box<dyn std::error::Error>> {
+    match game {
+        'w' => println!("Wordle game!"),
+        'c' => println!("Connections game!"),
+        _ => panic!("Invalid game :("),
+    }
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::blocking::get("https://www.nytimes.com/svc/wordle/v2/2025-11-15.json")?.json::<WordleResp>();
+    /*
+    let resp = reqwest::blocking::get(CONNECTIONS_URL)?.json::<ConnectionsResp>();
     match resp {
         Ok(response) => println!("{:#?}", response),
-        Err(_) => println!("ERROR"),
+        Err(e) => println!("ERROR: {:#?}", e),
     }
+    */
+
+    get_solution('a');
     Ok(())
 }
